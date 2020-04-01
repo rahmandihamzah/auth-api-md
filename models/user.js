f983efe9be96a294e406bb250d5e95b11cf538aa
rahmandihamzah <rahmandi.dev@gmail.com>
2020-04-01 19:25:37 +0700

    sequelize init db create model generate db migrate set unique constraint user email

---
+++ models/user.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  class User extends sequelize.Sequelize.Model {
+    static associate (models) {
+
+    }
+  }
+
+  User.init({
+    fullName: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notNull: {
+          args: true,
+          msg: 'Email sould not be empty'
+        },
+        isEmail: {
+          args: true,
+          msg : 'Incorrect email format'
+        },
+
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: 'Password sould not be empty'
+        },
+        len: {
+          args: [6],
+          msg: 'Minimum password length: 6'
+        }
+      }
+    }
+  }, {sequelize})
+  
+  return User;
+};
